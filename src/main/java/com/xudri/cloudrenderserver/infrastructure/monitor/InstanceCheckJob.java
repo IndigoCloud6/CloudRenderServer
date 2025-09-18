4063ce383683c4cc1f6249376a1bade7d4aeef12
IndigoCloud <maxyun@163.com>
2025-09-18 11:22:08 +0800

    加注解

--- src/main/java/com/xudri/cloudrenderserver/infrastructure/monitor/InstanceCheckJob.java
+++ src/main/java/com/xudri/cloudrenderserver/infrastructure/monitor/InstanceCheckJob.java
@@ -1,11 +1,13 @@
 package com.xudri.cloudrenderserver.infrastructure.monitor;
 
+import com.xudri.cloudrenderserver.common.util.LoggerUtil;
 import com.xudri.cloudrenderserver.core.client.ClientManager;
 import com.xudri.cloudrenderserver.core.streaming.PixelStreamingLauncher;
 import com.xudri.cloudrenderserver.core.streaming.PixelStreamingLauncherManager;
 import io.netty.channel.Channel;
 import jakarta.annotation.Resource;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 import java.util.Optional;
@@ -17,10 +19,11 @@ import java.util.Optional;
  * @Since 2025/9/17 14:20
  * @Version 1.0
  */
+@Component
 public class InstanceCheckJob {
 
     @Resource
-    private com.xudri.cloudrenderserver.core.streaming.PixelStreamingLauncherManager pixelStreamingLauncherManager;
+    private PixelStreamingLauncherManager pixelStreamingLauncherManager;
 
     @Resource
     private ClientManager clientManager;
@@ -28,6 +31,7 @@ public class InstanceCheckJob {
     @Scheduled(fixedRate = 10000)
     public void check() {
         Collection<PixelStreamingLauncher> launchers = pixelStreamingLauncherManager.getAllLaunchers();
+        LoggerUtil.logBusiness("InstanceCheckJob", "Checking %s instances", launchers.size());
         for (PixelStreamingLauncher launcher : launchers) {
             if (!launcher.isRunning()) {
                 Optional<Channel> streamerOpt = clientManager.getStreamerByInsId(launcher.getId());
